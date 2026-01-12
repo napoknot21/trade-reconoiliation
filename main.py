4bb337d314895ef98c10b6428a174a55ddae2a88
napoknot21 <napoknot21@protonmail.com>
2026-01-12 15:37:24 +0100

    First commit & structure added

---
+++ main.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+
+
+def main () :
+    """
+    Docstring for main
+    """
+
+    return None
+
+
+
+if __name__ == "__main__" :
+    """
+    """
+    main()
+    
